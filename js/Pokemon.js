67f5688cfa511c367985e2e1a6921704bbc5f924
23112001Davidluismora <davidluismora@gmail.com>
2024-09-29 23:02:44 +0100

    Documento y archivo todo del profe

--- js/Pokemon.js
+++ js/Pokemon.js
@@ -1,9 +1,11 @@
+// Creo clase
 export default class Pokemon {
-    constructor (data){
-        this.name = data.name;
-        this.id = data.id;
-        this.pkm_front = data.sprites.front_default;
-        this.pkm_back = data.sprites.back_default;
-        this.pkm_type = data.types;
+    // Creo el constructor
+    constructor(data){
+        this.name = data.name;  // Nombre del pokemon
+        this.id = data.id;      // Ide del pokemon
+        this.pkm_front = data.sprites.front_default; // Pokemon de frente
+        this.pkm_back = data.sprites.back_default;  // Pokemon de espalda
+        this.pkm_type = data.types;  // Tipo de pokemon
     }
 }
--- js/script.js
+++ js/script.js
@@ -1,110 +1,91 @@
-
+// Importamos la clase Pokemon
 import Pokemon from "./Pokemon.js";
 
+// Creo el array
 const pokemons = [];
 
 // Selecciono boton del DOM
 const button = document.querySelector("button");
 
-button.addEventListener("click", ()=> {
-    // alert("Me han pulsado. Hola!");
-    document.querySelectorAll("#filtro").forEach( (e) => {
-
-        // console.log(e)
+function showConsole() {
+    //Mostrar filtros
+    document.querySelectorAll("#filtro").forEach((e) => {
+        //console.log(e);
         e.style.visibility = "visible";
     });
 
-    document.querySelectorAll("#btn_lista_deseo").forEach( (e) => {
-                // console.log(e)
-                e.style.visibility = "visible";
+    // Mostrar botones
+    document.querySelectorAll("#btn_lista_deseo").forEach((e) => {
+        //console.log(e);
+        e.style.visibility = "visible";
     });
+}
 
-    document.querySelectorAll(".listaPokemon").forEach( (e) => {
-                // console.log(e)
-                e.style.visibility = "visible";
-            })
+button.addEventListener("click", () => {
+    // Mostrar lista Pokemon
+    document.querySelector("#pokedex").style.visibility = "visible";;
 
     startPokemon();
-
-
 
 });
 
 
-
-
 const startPokemon = async () => {
-
-    for(var i = 1; i<=151;i++){
-        try{
-            await fetch("https://pokeapi.co/api/v2/pokemon/"+i+"/")
-            .then(function(result){
-                return result.json();
-
-            })
-            .then(function(data){
-                console.log(data);
-                const pokemon = new Pokemon(data);
-                pushPokemon(pokemon);
-            })
-        } catch (error){
+    document.querySelector(".cargandoDatos").style.visibility = "visible";
+    for (var i = 1; i <= 151; i++) {
+        try {
+            await fetch("https://pokeapi.co/api/v2/pokemon/" + i + "/")
+                .then(function (result) {
+                    return result.json();
+                })
+                .then(function (data) {
+                    //console.log(data);
+                    const pokemon = new Pokemon(data); // Instancio la clase
+                    //console.log(pokemon);
+                    pushPokemon(pokemon); // Guardo pokemon en array
+                })
+        } catch (error) {
             alert(`There is am error: ${error}`)
-
         }
-
     }
 
     await showPokedex();
+
 };
 
-
-
-function pushPokemon (pokemon){
+function pushPokemon(pokemon) {
     pokemons.push(pokemon);
-};
-
+}
 
 const showPokedex = async () => {
-    // console.log(pokemon)
-    document.querySelector("#pokedex").style.visibility = "visable";
+    //console.log(pokemons);
+    // Hago visible la lista
+    document.querySelector("#pokedex").style.visibility = "visible";
 
     const pokedex = document.getElementById("pokedex");
 
-    for (var i=0; i< pokemons.length; i++){
+    document.querySelector(".cargandoDatos").style.visibility = "hidden";
+    showConsole();
+    button.style.visibility = "hidden";
+
+    for (var i = 0; i < pokemons.length; i++) {
         var aux = 0;
-        while (aux != pokemons[i].pkm_type.length){
-           if (aux == 0) var tipo1 = pokemons[i].pkm_type[aux].type.name;
-           if (aux ==1) var tipo2 = pokemons[i].pkm_type[aux].type.name;
-           else tipo2 = "";
-           aux++;
-
+        while (aux != pokemons[i].pkm_type.length) {
+            if (aux == 0) var tipo1 = pokemons[i].pkm_type[aux].type.name;
+            if (aux == 1) var tipo2 = pokemons[i].pkm_type[aux].type.name;
+            else tipo2 = "";
+            aux++;
         }
-        
-        const card = document.createElement("div");
-        card.classList.add("card")
-        
-
-
-        let random = parseInt(Math.random() * (1-1000+1)+1);
-        console.log(random)
-        pokedex.innerHTML += `<div class="card">
-                                    <img src="${pokemons[i].pkm_back}">
-                                    <img src="${pokemons[i].pkm_front}">
-                                    <br>
-                                    ${pokemons[i].id}. ${pokemons[i].name}
-                                    <br>
-                                    <div class="types">
-                                        ${tipo1} ${tipo2}
-                                    </div>
-                                    <div class="precies">
-                                        ${random * -1}
-                                    </div>
-                                </div>`
+        pokedex.innerHTML += `
+            <div class="card">
+               <img src="${pokemons[i].pkm_back}">
+               <img src="${pokemons[i].pkm_front}">
+               <br>
+               ${pokemons[i].id}. ${pokemons[i].name}
+               <br>
+               <div class="types">
+               ${tipo1}  ${tipo2} 
+               </div>
+            </div>`;
     }
-
-
 };
-
-function selected(){
-    alert("Hola")
-}
